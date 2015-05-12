96e7a05650ac7b40ba58df82b9242ff55fa5bc94
Sam Verschueren <sam.verschueren@gmail.com>
2015-05-12 13:33:25 +0200

    Added run.sh script that will install the typescript definitions

---
+++ run.sh
@@ -0,0 +1,33 @@
+# First make sure tsd is installed
+if ! type tsd &> /dev/null ; then
+    # Check if it is in repo
+    if ! $(npm_package_is_installed tsd) ; then
+        info "tsd not installed, trying to install it through npm"
+
+        sudo npm install -g --silent tsd
+        tsd_command="tsd"
+    else
+        info "tsd is available locally"
+        debug "tsd version: $(node ./node_modules/.bin/tsd --version)"
+        tsd_command="node ./node_modules/.bin/tsd"
+    fi
+else
+    # tsd is available globally
+    info "tsd is available"
+    debug "tsd version: $(tsd --version)"
+    tsd_command="tsd"
+fi
+
+# Reinstall the tsd files
+set +e
+$tsd_command reinstall
+result="$?"
+set -e
+
+# Fail if it is not a success or warning
+if [[ result -ne 0 && result -ne 6 ]]; then
+    warn "$result"
+    fail "tsd command failed"
+else
+    success "finished $tsd_command"
+fi
